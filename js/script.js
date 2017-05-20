a9f5cea16b0408e59c0529a9a577f111069acf8c
Durin Vladislav <rock_n_vladok@mail.ru>
2017-05-20 21:32:11 +0400

    Have been realized view and model objects

--- js/script.js
+++ js/script.js
@@ -4,9 +4,55 @@ var view = {
         messageArea.innerHTML = msg;
     },
     displayHit: function (location) {
-
+        var cell = document.getElementById(location);
+        cell.classList.add('hit');
     },
     displayMiss: function (location) {
-        
+        var cell = document.getElementById(location);
+        cell.classList.add('miss');
+    }
+};
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [
+        {
+            locations: ['06', '16', '26'], hits: ['', '', '']
+        },
+        {
+            locations: ['24', '34', '44'], hits: ['', '', '']
+        },
+        {
+            locations: ['10', '11', '12'], hits: ['', '', '']
+        }
+    ],
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = 'hit';
+                view.displayHit(guess);
+                view.displayMessage('Hit');
+                if (this.isSunk(ship)) {
+                    view.displayMessage('You sank my battleship!');
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage('You missed.');
+        return false;
+    },
+    isSunk: function (ship) {
+        for (var i = 0; i < ship.shipLength; i++) {
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        return true;
     }
 };
