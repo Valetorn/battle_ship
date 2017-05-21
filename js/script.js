0ba75fa4a093a355fe44a844de8b8865a67fb8e4
Durin Vladislav <rock_n_vladok@mail.ru>
2017-05-21 22:22:04 +0400

    Added controller object

--- js/script.js
+++ js/script.js
@@ -56,3 +56,44 @@ var model = {
         return true;
     }
 };
+var controller = {
+    guesses: 0,
+    processGuess: function (guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guess++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage('You sank all my battleships, in ' + this.guesses + 'guesses');
+            }
+        }
+    },
+};
+var parseGuess = function (guess) {
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    if (guess === null || guess.length !== 2) {
+        alert ('Oops, please enter a letter and a number on the board.');
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert('Oops, that isn\'t on the board.');
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert('Oops, that|\'s off the board!');
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
